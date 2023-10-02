5f9f7fc7e3896f0b8bc9b575ef63ddccf8693bb6
andrea <ngocanhhsgs@gmail.com>
2023-10-02 16:33:33 +0300

    minor change

---
+++ task.js
@@ -0,0 +1,17 @@
+// Exercise 1: declare 2 variables with names number1 and number 2 then apply Javascript operation: add, subtract, multiply, divide, get remainder to them
+
+// Exercise 2: wite a operation to convert the Celsius temperature to Fahrenheit.
+
+// declare a variable name : fullName then assign your name to its value.
+
+// using string method to get the length of variable fullName,
+
+// using string method to uppercase the variable
+
+// using string method to lowercase the variable
+
+// using string method to slice the variable
+
+// using string method to check which letter has index 3
+
+// using string method to get the letter has index between 1 and 3
